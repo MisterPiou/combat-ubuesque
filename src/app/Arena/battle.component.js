bc3743eb9b707e7fc34ab553bd5344167181fd5a
MrPiou <mrpiou34@gmail.com>
2017-05-30 11:26:28 +0200

    Premier essai pour le coup de base dans combat

--- src/app/Arena/battle.component.js
+++ src/app/Arena/battle.component.js
@@ -16,14 +16,47 @@ var BattleComponent = (function () {
     function BattleComponent(route, heroService) {
         this.route = route;
         this.heroService = heroService;
+        /* Attaque */
+        this.attacksPercentages = [0, 0, 0];
+        this.heroLifePercentage = 100;
+        this.opponentLifePercentage = 100;
+        /* Global */
+        this.intervalId = 0;
     }
     BattleComponent.prototype.ngOnInit = function () {
         var _this = this;
         this.route.params.subscribe(function (params) {
             if (+params['id'] == 0) {
                 _this.opponent = new hero_1.Hero(0, 0, "Pouchink Paul", 0, 0, 0, 1, 100);
+                _this.opponentLifeActual = _this.opponent.life;
             }
         });
+        this.hero = new hero_1.Hero(1, 1, "Joueur", 0, 0, 0, 1, 100);
+        this.heroLifeActual = this.hero.life;
+    };
+    BattleComponent.prototype.onAttack = function (spell) {
+        if (spell == 0 & this.attacksPercentages[spell] == 0) {
+            this.opponentLoseLife(Math.floor((Math.random() * 5) + 5 + 1));
+            this.coolDown(spell, 3000);
+        }
+    };
+    BattleComponent.prototype.opponentLoseLife = function (lifeLose) {
+        this.opponentLifeActual -= lifeLose;
+        this.opponentLifePercentage = (this.opponentLifeActual / this.opponent.life) * 100;
+    };
+    BattleComponent.prototype.clearTimer = function () { clearInterval(this.intervalId); };
+    BattleComponent.prototype.coolDown = function (attack, time) {
+        var _this = this;
+        this.clearTimer();
+        this.attacksPercentages[attack] = 100;
+        var interval = 100 / (time / 200);
+        this.intervalId = window.setInterval(function () {
+            _this.attacksPercentages[attack] -= interval;
+            if (_this.attacksPercentages[attack] < 0) {
+                _this.attacksPercentages[attack] = 0;
+                _this.clearTimer();
+            }
+        }, 200);
     };
     return BattleComponent;
 }());
--- src/app/Arena/battle.component.ts
+++ src/app/Arena/battle.component.ts
@@ -10,7 +10,17 @@ import {HeroService}    from '../Hero/hero.service';
 })
 export class BattleComponent implements OnInit
 {
+    /* Attaque */
+    attacksPercentages = [0,0,0];
+    /* Personnage */
+    hero: Hero;
+    heroLifeActual: number;
+    heroLifePercentage = 100;
     opponent: Hero;
+    opponentLifeActual: number;
+    opponentLifePercentage = 100;
+    /* Global */
+    intervalId = 0;
     
     constructor(
         private route: ActivatedRoute,
@@ -21,7 +31,38 @@ export class BattleComponent implements OnInit
         this.route.params.subscribe((params: Params) => {
             if(+params['id']==0) {
                 this.opponent = new Hero(0, 0, "Pouchink Paul", 0, 0, 0, 1, 100);
+                this.opponentLifeActual = this.opponent.life;
             }
         });
+        this.hero = new Hero(1, 1, "Joueur", 0, 0, 0, 1, 100);
+        this.heroLifeActual = this.hero.life;
     }
+    
+    onAttack(spell: number) {
+        if(spell==0 & this.attacksPercentages[spell] == 0) {
+            this.opponentLoseLife(Math.floor((Math.random() * 5) + 5 + 1));
+            this.coolDown(spell, 3000);
+        }
+    }
+    
+    private opponentLoseLife(lifeLose: number) {
+        this.opponentLifeActual -= lifeLose;
+        this.opponentLifePercentage = (this.opponentLifeActual / this.opponent.life ) * 100;
+    }
+    
+    clearTimer() { clearInterval(this.intervalId); }
+    
+    private coolDown(attack: number, time: number) {
+        this.clearTimer();
+        this.attacksPercentages[attack] = 100;
+        let interval = 100 / (time / 200);
+        this.intervalId = window.setInterval(() => {
+            this.attacksPercentages[attack] -= interval;
+            if(this.attacksPercentages[attack] < 0) {
+                this.attacksPercentages[attack] = 0;
+                this.clearTimer();
+            }
+        }, 200);
+    }
+    
 }
