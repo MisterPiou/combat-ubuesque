d5ec2cb1657020c4218ac297de18707e60431981
MrPiou <mrpiou34@gmail.com>
2017-08-10 13:26:58 +0200

    bug fix deploiement

--- src/app/data.js
+++ src/app/data.js
@@ -1,4 +1,4 @@
 "use strict";
-exports.url_root = "http://192.168.1.6";
+exports.url_root = "http://192.168.1.88";
 exports.url_base = exports.url_root + "/combat-ubuesque/backend/web/";
 //# sourceMappingURL=data.js.map
--- src/app/data.ts
+++ src/app/data.ts
@@ -1,2 +1,2 @@
-export const url_root = "http://192.168.1.88";
-export const url_base = url_root+"/combat-ubuesque/backend/web/";
+export const url_root = "http://192.168.1.6";
+export const url_base = "http://192.168.1.88/combat-ubuesque/backend/web/app.php/";
